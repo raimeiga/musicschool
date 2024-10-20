c82047d549e2e37d4c8fa4ccb2b31a55f09a00d3
raimeiga <yusuke612@hotmail.com>
2024-10-21 01:11:25 +0900

    調整

---
+++ archive-blog.php
@@ -0,0 +1,50 @@
+<?php get_header(); ?>
+
+<a href="#" class="c-arrow">
+    <p class="c-arrow__img">
+        <img src="./images/common/arrow-t.svg" alt="△">
+    </p>
+</a>
+
+<?php get_template_part('template-parts/contact-button'); ?>
+
+<div class="c-headline headline--blog-list">
+    <p class="c-headline-text">ブログ</p>
+</div>
+
+<div class="c-breadcrumbs">
+    <div class="c-breadcrumbs-text-area">
+        <a href="index.html" class="c-breadcrumbs-text">ホーム</a>
+        <p class="c-breadcrumbs-sign">&#62;</p>
+        <p class="c-breadcrumbs-text">ブログ</p>
+    </div>
+</div>
+
+<section class="blog-list">
+    <div class="content-area">
+        <h2 class="c-head-lower">ブログ一覧</h2>
+        
+        <a href="./blog-details.html" class="c-blog-block c-blog-block--list">
+            <div class="c-blog-block__img-area c-blog-block__img-area--list">
+                <p class="c-blog-block__img c-blog-block__img--list"><img srcset="./images/top/blog01.png 1x, ./images/top/blog01@2x.png 2x" alt=""></p>
+                <p class="c-blog-block__tag">ギター</p>
+            </div>
+            <div class="c-blog-block__text-area">
+                <p class="c-blog-block__title c-blog-block__title--list">アルペジオが劇的に向上する3つの習慣</p>
+                <p class="c-blog-block__date c-blog-block__date--list"><time datetime="2022-01-01">0000.00.00</time></p>
+                <p class="c-blog-block__text">本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。</p>
+            </div>
+        </a>
+    </div>
+    <div class="content-area content-area-blog-pager">
+        <div class="c-pager">
+            <a href="" class="c-pager__item c-pager__item--active">1</a>
+            <a href="" class="c-pager__item">2</a>
+            <a href="" class="c-pager__item">3</a>
+            <p class="c-pager__leader">⋯</p>
+            <a href="" class="c-pager__item c-pager__item--last">9</a>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
