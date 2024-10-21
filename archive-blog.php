903954a9ea8144105af026a703f82cee381daa11
raimeiga <yusuke612@hotmail.com>
2024-10-22 01:03:40 +0900

    調整

--- archive-blog.php
+++ archive-blog.php
@@ -23,8 +23,106 @@
 <section class="blog-list">
     <div class="content-area">
         <h2 class="c-head-lower">ブログ一覧</h2>
-        
         <a href="./blog-details.html" class="c-blog-block c-blog-block--list">
+            <div class="c-blog-block__img-area c-blog-block__img-area--list">
+                <p class="c-blog-block__img c-blog-block__img--list"><img srcset="./images/top/blog01.png 1x, ./images/top/blog01@2x.png 2x" alt=""></p>
+                <p class="c-blog-block__tag">ギター</p>
+            </div>
+            <div class="c-blog-block__text-area">
+                <p class="c-blog-block__title c-blog-block__title--list">アルペジオが劇的に向上する3つの習慣</p>
+                <p class="c-blog-block__date c-blog-block__date--list"><time datetime="2022-01-01">0000.00.00</time></p>
+                <p class="c-blog-block__text">本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。</p>
+            </div>
+        </a>
+        <a href="./blog-details.html" class="c-blog-block c-blog-block--list">
+            <div class="c-blog-block__img-area c-blog-block__img-area--list">
+                <p class="c-blog-block__img c-blog-block__img--list"><img srcset="./images/top/blog02.png 1x, ./images/top/blog02@2x.png 2x" alt=""></p>
+                <p class="c-blog-block__tag">集客方法</p>
+            </div>
+            <div class="c-blog-block__text-area">
+                <p class="c-blog-block__title c-blog-block__title--list">集客してる間は売れないという法則</p>
+                <p class="c-blog-block__date c-blog-block__date--list"><time datetime="2022-01-01">0000.00.00</time></p>
+                <p class="c-blog-block__text">本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。</p>
+            </div>
+        </a>
+        <a href="./blog-details.html" class="c-blog-block c-blog-block--list">
+            <div class="c-blog-block__img-area c-blog-block__img-area--list">
+                <p class="c-blog-block__img c-blog-block__img--list"><img srcset="./images/top/blog03.png 1x, ./images/top/blog03@2x.png 2x" alt=""></p>
+                <p class="c-blog-block__tag">SNS</p>
+            </div>
+            <div class="c-blog-block__text-area">
+                <p class="c-blog-block__title c-blog-block__title--list">フォロワーではなくファンを増やせとは？</p>
+                <p class="c-blog-block__date c-blog-block__date--list"><time datetime="2022-01-01">0000.00.00</time></p>
+                <p class="c-blog-block__text">本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。</p>
+            </div>
+        </a>
+        <a href="./blog-details.html" class="c-blog-block c-blog-block--list">
+            <div class="c-blog-block__img-area c-blog-block__img-area--list">
+                <p class="c-blog-block__img c-blog-block__img--list"><img srcset="./images/top/blog01.png 1x, ./images/top/blog01@2x.png 2x" alt=""></p>
+                <p class="c-blog-block__tag">ギター</p>
+            </div>
+            <div class="c-blog-block__text-area">
+                <p class="c-blog-block__title c-blog-block__title--list">アルペジオが劇的に向上する3つの習慣</p>
+                <p class="c-blog-block__date c-blog-block__date--list"><time datetime="2022-01-01">0000.00.00</time></p>
+                <p class="c-blog-block__text">本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。</p>
+            </div>
+        </a>
+        <a href="./blog-details.html" class="c-blog-block c-blog-block--list">
+            <div class="c-blog-block__img-area c-blog-block__img-area--list">
+                <p class="c-blog-block__img c-blog-block__img--list"><img srcset="./images/top/blog02.png 1x, ./images/top/blog02@2x.png 2x" alt=""></p>
+                <p class="c-blog-block__tag">集客方法</p>
+            </div>
+            <div class="c-blog-block__text-area">
+                <p class="c-blog-block__title c-blog-block__title--list">集客してる間は売れないという法則</p>
+                <p class="c-blog-block__date c-blog-block__date--list"><time datetime="2022-01-01">0000.00.00</time></p>
+                <p class="c-blog-block__text">本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。</p>
+            </div>
+        </a>
+        <a href="./blog-details.html" class="c-blog-block c-blog-block--list">
+            <div class="c-blog-block__img-area c-blog-block__img-area--list">
+                <p class="c-blog-block__img c-blog-block__img--list"><img srcset="./images/top/blog03.png 1x, ./images/top/blog03@2x.png 2x" alt=""></p>
+                <p class="c-blog-block__tag">SNS</p>
+            </div>
+            <div class="c-blog-block__text-area">
+                <p class="c-blog-block__title c-blog-block__title--list">フォロワーではなくファンを増やせとは？</p>
+                <p class="c-blog-block__date c-blog-block__date--list"><time datetime="2022-01-01">0000.00.00</time></p>
+                <p class="c-blog-block__text">本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。</p>
+            </div>
+        </a>
+        <a href="./blog-details.html" class="c-blog-block c-blog-block--list">
+            <div class="c-blog-block__img-area c-blog-block__img-area--list">
+                <p class="c-blog-block__img c-blog-block__img--list"><img srcset="./images/top/blog01.png 1x, ./images/top/blog01@2x.png 2x" alt=""></p>
+                <p class="c-blog-block__tag">ギター</p>
+            </div>
+            <div class="c-blog-block__text-area">
+                <p class="c-blog-block__title c-blog-block__title--list">アルペジオが劇的に向上する3つの習慣</p>
+                <p class="c-blog-block__date c-blog-block__date--list"><time datetime="2022-01-01">0000.00.00</time></p>
+                <p class="c-blog-block__text">本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。</p>
+            </div>
+        </a>
+        <a href="./blog-details.html" class="c-blog-block c-blog-block--list">
+            <div class="c-blog-block__img-area c-blog-block__img-area--list">
+                <p class="c-blog-block__img c-blog-block__img--list"><img srcset="./images/top/blog02.png 1x, ./images/top/blog02@2x.png 2x" alt=""></p>
+                <p class="c-blog-block__tag">集客方法</p>
+            </div>
+            <div class="c-blog-block__text-area">
+                <p class="c-blog-block__title c-blog-block__title--list">集客してる間は売れないという法則</p>
+                <p class="c-blog-block__date c-blog-block__date--list"><time datetime="2022-01-01">0000.00.00</time></p>
+                <p class="c-blog-block__text">本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。</p>
+            </div>
+        </a>
+        <a href="./blog-details.html" class="c-blog-block c-blog-block--list">
+            <div class="c-blog-block__img-area c-blog-block__img-area--list">
+                <p class="c-blog-block__img c-blog-block__img--list"><img srcset="./images/top/blog03.png 1x, ./images/top/blog03@2x.png 2x" alt=""></p>
+                <p class="c-blog-block__tag">SNS</p>
+            </div>
+            <div class="c-blog-block__text-area">
+                <p class="c-blog-block__title c-blog-block__title--list">フォロワーではなくファンを増やせとは？</p>
+                <p class="c-blog-block__date c-blog-block__date--list"><time datetime="2022-01-01">0000.00.00</time></p>
+                <p class="c-blog-block__text">本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。</p>
+            </div>
+        </a>
+        <a href="./blog-details.html" class="c-blog-block c-blog-block--list u-item-bottom">
             <div class="c-blog-block__img-area c-blog-block__img-area--list">
                 <p class="c-blog-block__img c-blog-block__img--list"><img srcset="./images/top/blog01.png 1x, ./images/top/blog01@2x.png 2x" alt=""></p>
                 <p class="c-blog-block__tag">ギター</p>
