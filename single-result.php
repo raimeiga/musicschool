1f134a26dd6eb8cfda8cc7b2d53c8fe56fdcc0a5
raimeiga <yusuke612@hotmail.com>
2024-10-26 17:11:12 +0900

    9-6：記事を表示しよう　終了

--- single-result.php
+++ single-result.php
@@ -9,61 +9,56 @@
 <!-- 問い合わせボタンの読み込み -->
 <?php get_template_part('template-parts/contact-button'); ?>
 
-<div class="c-breadcrumbs">
-    <div class="c-breadcrumbs-text-area">
-        <a href="index.html" class="c-breadcrumbs-text">ホーム</a>
-        <p class="c-breadcrumbs-sign">&#62;</p>
-        <a href="result-list.html" class="c-breadcrumbs-text">卒業実績</a>
-        <p class="c-breadcrumbs-sign">&#62;</p>
-        <a href="result-list.html" class="c-breadcrumbs-text">ポップス</a>
-        <p class="c-breadcrumbs-sign">&#62;</p>
-        <p class="c-breadcrumbs-text">タイトルが入ります。タイトルが入ります。タイトルが入ります。</p>
-    </div>
-</div>
+<!-- パンくずリストの読み込み -->
+<?php get_template_part('template-parts/breadcrumbs'); ?>
 
+<?php while ( have_posts() ): the_post(); ?>
 <section class="result-details">
     <div class="content-area content-area--result-details">
-        <div class="c-col-main c-col-main--result">
-            <div class="col-main-body">
-                <div class="c-result-block__img-area c-result-block__img-area--detail-top">
-                    <p class="c-result-block__img c-result-block__img--detail-top"><img srcset="./images/result/details-top.png 1x, ./images/result/details-top@2x.png 2x" alt=""></p>
-                    <p class="c-result-block__tag">ポップス</p>
-                </div>
-                <div class="c-result-details-title-area">
-                    <h1 class="c-result-details-title">タイトルが入ります。タイトルが入ります。タイトルが入ります。</h1>
-                    <p class="c-result-details-time"><time datetime="2022-01-01">0000.00.00</time></p>
-                </div>
-                <div class="c-result-details-body">
-                    <div class="c-result-details-body__table-area">
-                        <table class="c-result-details-body__table">
-                            <tr>
-                                <th class="c-result-details-body__table-head">名前</th>
-                                <td class="c-result-details-body__table-data">丸山</td>
-                            </tr>
-                            <tr>
-                                <th class="c-result-details-body__table-head">職業</th>
-                                <td class="c-result-details-body__table-data">証券会社勤務</td>
-                            </tr>
-                            <tr>
-                                <th class="c-result-details-body__table-head">ジャンル</th>
-                                <td class="c-result-details-body__table-data">入力欄</td>
-                            </tr>
-                            <tr>
-                                <th class="c-result-details-body__table-head">実績</th>
-                                <td class="c-result-details-body__table-data">入力欄</td>
-                            </tr>
-                            <tr>
-                                <th class="c-result-details-body__table-head">SNS</th>
-                                <td class="c-result-details-body__table-data">入力欄</td>
-                            </tr>
-                        </table>
-                    </div>
-                    <div class="c-result-details-body__text-area">
-                        <p>昔やっていた音楽活動で、副収入が得られるようになったので、毎日充実するようになりました。</p>
-                    </div>
-                </div>
-            </div>
+    <div class="c-col-main c-col-main--result">
+      <div class="col-main-body">
+        <div class="c-result-block__img-area c-result-block__img-area--detail-top">
+          <p class="c-result-block__img c-result-block__img--detail-top"><?php the_post_thumbnail('large'); ?></p>
+          <p class="c-result-block__tag">
+            <?php if( has_term('', 'result_tag', $post->ID) ): ?> 
+            <?php $post_term = get_the_terms($post->ID, 'result_tag'); echo $post_term[0]->name; ?> <?php endif; ?>
+          </p>
         </div>
+        <div class="c-result-details-title-area">
+          <h1 class="c-result-details-title"><?php the_title(); ?></h1>
+          <p class="c-result-details-time"><time datetime="the_time('Y-m-d')"><?php the_time('Y.m.d'); ?></time></p>
+       </div>
+        <div class="c-result-details-body">
+          <div class="c-result-details-body__table-area">
+            <table class="c-result-details-body__table">
+              <tr>
+                <th class="c-result-details-body__table-head">名前</th>
+                <td class="c-result-details-body__table-data"><?php echo get_post_meta($post->ID, '名前', true); ?></td>
+              </tr>
+              <tr>
+                <th class="c-result-details-body__table-head">職業</th>
+                <td class="c-result-details-body__table-data"><?php echo get_post_meta($post->ID, '職業', true); ?></td>
+              </tr>
+              <tr>
+                <th class="c-result-details-body__table-head">ジャンル</th>
+                <td class="c-result-details-body__table-data"><?php echo get_post_meta($post->ID, 'ジャンル', true); ?></td>
+              </tr>
+              <tr>
+                <th class="c-result-details-body__table-head">実績</th>
+                <td class="c-result-details-body__table-data"><?php echo get_post_meta($post->ID, '実績', true); ?></td>
+              </tr>
+              <tr>
+                <th class="c-result-details-body__table-head">SNS</th>
+                <td class="c-result-details-body__table-data"><?php echo get_post_meta($post->ID, 'sns', true); ?></td>
+              </tr>
+            </table>
+          </div>
+          <div class="c-result-details-body__text-area">
+            <?php the_content(); ?>
+          </div>
+        </div>
+      </div>
+</div>
         <div class="c-col-main--arrow">
             <a href="" class="c-col-main--arrow-prev">
                 <p class="c-blog-section-name c-blog-section-name--arrow-prev">◀︎ 前の記事</p>
@@ -123,5 +118,6 @@
         </div>
     </div>
 </section>
+<?php endwhile; ?>
 
 <?php get_footer(); ?>
