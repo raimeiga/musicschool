1b9a789beea2384818522e01c7ecfa800d95ca98
raimeiga <yusuke612@hotmail.com>
2024-11-01 00:06:03 +0900

    page-contact-send.php作成して、ヘッダー、フッター、パンくずリストのテンプレパーツ読み込みのコードを記述

---
+++ page-contact-send.php
@@ -0,0 +1,24 @@
+<?php  get_header(); ?>
+
+<a href="#" class="c-arrow">
+    <p class="c-arrow__img">
+        <img src="./images/common/arrow-t.svg" alt="△">
+    </p>
+</a>
+
+<div class="c-headline headline--notice">
+    <p class="c-headline-text">お問い合わせ</p>
+</div>
+
+<!-- パンくずリストの読み込み -->
+<?php get_template_part('template-parts/breadcrumbs'); ?>
+
+
+<section class="c-message">
+    <div class="content-area content-area--contact-form">
+        <p class="c-message-description">お問い合わせいただきありがとうございました。<br>内容確認後、担当者よりメールにてご連絡いたします。</p>
+        <a href="./index.html" class="c-button c-button--main">ホームへ戻る</a>
+    </div>
+</section>
+
+<?php get_footer(); ?>
