09aa495883198ff2d71c6e880824c055ae4c5574
raimeiga <yusuke612@hotmail.com>
2024-11-01 00:16:12 +0900

    12-6：お問い合わせボタンのパスを修正しよう

--- template-parts/contact-button.php
+++ template-parts/contact-button.php
@@ -1,1 +1,1 @@
-<a href="<?php echo get_template_directory_uri(); ?>/contact-form.html" class="c-button c-button--bottom">お問い合わせ</a>
+<a href="<?php echo esc_url( home_url( '/' ) ); ?>contact" class="c-button c-button--bottom">お問い合わせ</a>
