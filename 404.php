bed34762bee2eceaa7a72a76537c4945a73c041e
raimeiga <yusuke612@hotmail.com>
2024-11-02 15:13:03 +0900

    ホームへ戻るボタンのパスが静的ページへのリンクのままになっているので、正しく修正

--- 404.php
+++ 404.php
@@ -13,8 +13,7 @@
 <section class="c-message">
     <div class="content-area content-area--404">
         <p class="c-message-description">申し訳ございませんが、お探しのページが見つかりませんでした。<br>お探しのページは一時的に表示ができない状態にあるか、移動または削除された可能性があります。</p>
-        <a href="./index.html" class="c-button c-button--main">ホームへ戻る</a>
-    </div>
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="c-button c-button--main">ホームへ戻る</a>    </div>
 </section>
 
 <?php get_footer(); ?>
