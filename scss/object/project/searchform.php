6ea9f11f7d60930a4fa4b012b1b2a13a726b8fb1
raimeiga <yusuke612@hotmail.com>
2024-11-02 14:45:43 +0900

    search.htmlからsearch.phpに変更

---
+++ scss/object/project/searchform.php
@@ -0,0 +1,4 @@
+<form action="./search.html" class="c-blog-section__form">
+                    <input type="search" class="c-blog-section__form-input">
+                    <input type="image" src="./images/common/icon-search.svg" class="c-blog-section__form-submit">
+                </form>
