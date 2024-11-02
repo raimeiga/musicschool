c83f23d20af73549ad91822a66f71b4a8cc080f5
raimeiga <yusuke612@hotmail.com>
2024-11-02 15:48:50 +0900

    18-2：タイトルとディスクリプションを設定しよう　OK

--- header.php
+++ header.php
@@ -1,13 +1,77 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php if(is_front_page()): ?>
     <title>きたむらミュージックスクール | 「音楽で生きる」を叶える ミュージックスクール</title>
-    <meta name="description" content="「音楽で生きる」を叶える ミュージックスクール「きたむらミュージックスクール」の公式ホームページです。">
-    
-    <?php wp_head(); ?>
+    <meta name="description" content="「音楽で生きる」を叶える ミュージックスクール
+    「きたむらミュージックスクール」の公式ホームページです。">
+  <!-- 固定ページ -->
+  <?php elseif(is_page()): ?>
+    <?php if(get_field("title")): ?>
+    <title><?php echo the_field('title'); ?></title>
+    <?php else: ?>
+    <title><?php the_title(); ?> | きたむらミュージックスクール</title>
+    <?php endif; ?>
+    <?php if(get_field("description")): ?>
+    <meta name="description" content="<?php echo the_field('description'); ?>">
+    <?php else: ?>
+    <meta name="description" content="きたむらミュージックスクール公式ホームページの
+    <?php the_title(); ?>ページです。">
+    <?php endif; ?>
+  <!-- 投稿ページ -->
+  <?php elseif(is_single()): ?>
+    <?php if(get_field("title")): ?>
+    <title><?php echo the_field('title'); ?></title>
+    <?php else: ?>
+    <title><?php the_title(); ?> | きたむらミュージックスクール</title>
+    <?php endif; ?>
+    <?php if(get_field("description")): ?>
+    <meta name="description" content="<?php echo the_field('description'); ?>">
+    <?php else: ?>
+    <meta name="description" content="<?php if(have_posts()): ?><?php while(have_posts()): the_post(); ?>
+    <?php 
+    $des = get_the_content();
+    $des = strip_tags($des);
+    $des = str_replace(array("\r\n"," "), '', $des);
+    $desp = mb_substr($des, 0, 120, "UTF-8");
+    echo $desp;
+    ?>
+    <?php endwhile; ?><?php endif; ?>">
+    <?php endif; ?>
+  <!-- アーカイブページ -->
+  <?php elseif(is_archive()): ?>
+    <?php if(!is_paged()): ?>
+    <?php
+    $cat_id = get_queried_object()->cat_ID;
+    $post_id = 'category_'.$cat_id;
+    ?>
+    <title><?php single_cat_title( '', true ); ?> | きたむらミュージックスクール</title>
+    <meta name="description" content="きたむらミュージックスクール公式ホームページの
+    <?php single_cat_title( '', true ); ?>カテゴリアーカイブページです。">
+    <?php else: ?>
+    <title><?php single_cat_title( '', true ); ?>カテゴリアーカイブ 
+    <?php show_page_number(''); ?>ページ目  | きたむらミュージックスクール</title>
+    <meta name="description" content="きたむらミュージックスクール公式ホームページの
+    <?php single_cat_title( '', true ); ?>カテゴリアーカイブ <?php show_page_number(''); ?>ページ目です。">
+    <?php endif; ?>
+  <!-- 検索結果ページ -->
+  <?php elseif(is_search()): ?>
+    <title>検索結果 | きたむらミュージックスクール</title>
+    <meta name="description" content="きたむらミュージックスクール公式ホームページの検索結果ページです。">
+  <!-- 404ページ -->
+  <?php elseif(is_404()): ?>
+    <title>お探しのページはございません | きたむらミュージックスクール</title>
+    <meta name="description" content="きたむらミュージックスクール公式ホームページの404ページです。">
+  <!-- その他 -->
+  <?php else: ?>
+    <title><?php the_title(); ?> | きたむらミュージックスクール</title>
+    <meta name="description" content="きたむらミュージックスクール公式ホームページの
+    <?php the_title(); ?>ページです。">
+  <?php endif; ?>
+  <?php wp_head(); ?>
 </head>
 
 <body style="display: none;">
